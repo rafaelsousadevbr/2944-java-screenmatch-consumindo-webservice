11c3101c3ec7df684707ef86dcc34fe85d7a3041
rafaelsousadevbr <rafael@sousalima.dev.br>
2026-02-20 03:51:58 -0300

    Foi criado o Record TituloOmdb

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -18,6 +18,12 @@ public class Titulo implements Comparable<Titulo> {
         this.anoDeLancamento = anoDeLancamento;
     }
 
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
+    }
+
     public String getNome() {
         return nome;
     }
